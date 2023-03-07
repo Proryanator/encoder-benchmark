05cc610172822570756c7921b1acd37492de9da3
Ryan Laseter <proryanator@gmail.com>
2023-03-07 07:56:38 -0600

    Catching error on non-Nvidia GPU systems

--- gpus/src/lib.rs
+++ gpus/src/lib.rs
@@ -3,7 +3,14 @@ use nvml_wrapper::Nvml;
 pub mod device;
 
 pub fn get_gpus() -> Vec<String> {
-    let nvml = Nvml::init().unwrap();
+    let nvml = match Nvml::init() {
+        Ok(nvml) => { nvml }
+        Err(_) => {
+            println!("Unable to auto-detect multiple GPU's, falling back to using first GPU or provided one via '-gpu' option if specified");
+            return Vec::new();
+        }
+    };
+
     let device_count = nvml.device_count().unwrap();
 
     let mut list = Vec::new();
